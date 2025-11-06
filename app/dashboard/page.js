e0d28a2cacd33141b3ac67120572372b9b87c668
Abdelbosspie <98158507+Abdelbosspie@users.noreply.github.com>
2025-11-06 20:28:02 +0000

    update

--- app/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-import Overview from "./overview/page";
-
-export default function Page() {
-  return <Overview />;
-}
